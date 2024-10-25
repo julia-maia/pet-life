1086905d2dcf0a4130fe1092adc1b4aee30ea758
julia-maia <brambatti1212@gmail.com>
2024-10-24 23:52:08 -0300

    Add files via upload

---
+++ PetLife/index.js
@@ -0,0 +1,6 @@
+var elementos = document.querySelectorAll(".duvida");
+elementos.forEach(function (duvida) {
+  duvida.addEventListener("click", function () {
+    duvida.classList.toggle("ativa");
+  });
+});
